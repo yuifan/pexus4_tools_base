b44b2f64a3fd014ea0676edb2dac0145cb52b6cf
yuifan.park <yuifan.park@lge.com>
2012-11-23 11:12:44 +0900

    Add TestClass

---
+++ TestClass.cpp
@@ -0,0 +1,15 @@
+#include "TestClass.h"
+#include <iostream>
+
+TestClass::TestClass() {
+    first = 0;
+    second = 0;
+}
+
+TestClass::TestClass(int first, int second) {
+    this.first = first;
+    this.second = second;
+}
+
+TestClass::~TestClass() {
+}
