14f5be3124588313dbcffdd0f852ee64d6f38b4f
yuifan.park <yuifan.park@lge.com>
2012-11-26 16:04:37 +0900

    Modify HelloWorld2

--- HelloWorld2/HelloWorld2.cpp
+++ HelloWorld2/HelloWorld2.cpp
@@ -1,8 +1,10 @@
 #include "HelloWorld2.h"
 
 HelloWorld2::HelloWorld2() {
-    hello = "Hello";
-    world = "World";
+    this->hello = new char[strlen("Hello") + 1];
+    this->world = new char[strlen("World") + 1];
+    strcpy(this->hello, "Hello");
+    strcpy(this->world, "World");
 }
 
 HelloWorld2::HelloWorld2(char* hello, char* world) {
@@ -12,17 +14,17 @@ HelloWorld2::HelloWorld2(char* hello, char* world) {
     strcpy(this->world, world);
 }
 
-HelloWorld2::HelloWorld2(const HelloWorld2& string) {
-    hello = new char[strlen(string.hello) + 1];
-    world = new char[strlen(string.world) + 1];
+HelloWorld2::HelloWorld2(const HelloWorld2& v) {
+    hello = new char[strlen(v.hello) + 1];
+    world = new char[strlen(v.world) + 1];
 
-    strcpy(string.hello, this->hello);
-    strcpy(string.world, this->world);
+    strcpy(hello, v.hello);
+    strcpy(world, v.world);
 }
 
 HelloWorld2::~HelloWorld2() {
-    delete this->hello;
-    delete this->world;
+    delete[] this->hello;
+    delete[] this->world;
 }
 
 void HelloWorld2::printString() {
@@ -32,15 +34,15 @@ void HelloWorld2::printString() {
 int main(void) {
     HelloWorld2* hw1 = new HelloWorld2();
     HelloWorld2* hw2 = new HelloWorld2("Wonderful", "World!!");
-    HelloWorld2* hw3 = hw2;
+    //HelloWorld2 hw3 = hw2;
 
     hw1->printString();
     hw2->printString();
-    hw3->printString();
+    //hw3->printString();
 
     delete hw1;
     delete hw2;
-    delete hw3;
+    //delete hw3;
 
     return 0;
 } 
