b512f379fda4d39f3d2490bb9247b7b878622cdb
yuifan.park <yuifan.park@lge.com>
2012-11-27 10:35:03 +0900

    Add CarClass, Modify HelloWorld

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
+int *crashAddress() {
+     return (int *) 0xDEADBEEF;
+}
+
+void crashTest1() {
+     int value = 0;
+     int * crash = crashAddress();
+     cout << *crash << endl;
+}
+
 int main(void) {
     cout<<"Hello World!!"<<endl;
     cout<<"Hello World!!"<<endl;
+    crashTest1();
     return 0;
 }
